7665a4f4b83e20d1449bc75a5a89af6cae840369
DESKTOP-43O7LPO\baekj <baekjs0123@naver.com>
2022-04-06 18:45:46 +0900

    메모 인터셉터

--- src/main/java/com/memo/config/WebMvcConfig.java
+++ src/main/java/com/memo/config/WebMvcConfig.java
@@ -1,13 +1,19 @@
 package com.memo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.memo.common.FileManagerService;
+import com.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private PermissionInterceptor interceptor;
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -16,4 +22,15 @@ public class WebMvcConfig implements WebMvcConfigurer{
 		.addResourceLocations("file:///" + FileManagerService.FILE_UPLOAD_PATH); // 실제 파일 저장 위치
 		
 	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+		.addInterceptor(interceptor)
+		.addPathPatterns("/**")  // ** 아래 디렉토리까지 모두 확인
+		.excludePathPatterns("/error", "/static/**", "/user/sign_out"); // 예외처리 (인터셉터 안타게)
+	}
 }
+
+
+
